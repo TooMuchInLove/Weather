f575ef35c356d859be19f4af552fb328dcd09a55
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-19 03:27:18 +0300

    обновление gps (разделение задач)

--- weather_gps.py
+++ weather_gps.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
 # Аннотация типов
-from typing import Tuple, NamedTuple
+from typing import NamedTuple
 # OpenStreetMap Nominatim API для получения широты и долготы из физического адреса
 from geopy.geocoders import Nominatim
 
 # Локальный config
 from config import LOCATION_STORAGE
+# Локальный исключения
+from exceptions import ErrorCountryDoesNotExist
+from exceptions import ErrorCityDoesNotExist
+from exceptions import ErrorCantGetCoordinates
 
 # ============================================= #
 # ======= Подсказка типов Type Hinting ======== #
@@ -15,9 +19,10 @@ from config import LOCATION_STORAGE
 COUNTRY = str
 # Название города
 CITY = str
-# Название локации (страна + город)
-LOCATION = Tuple[COUNTRY, CITY]
-
+# Координаты широта и долгота (Именованный кортеж)
+class Coordinates(NamedTuple):
+    latitude: float
+    longitude: float
 
 # ============================================= #
 # ======= Глобальный переменные =============== #
@@ -26,53 +31,72 @@ LOCATION = Tuple[COUNTRY, CITY]
 NAME_COUNTRY = str
 # Название города
 NAME_CITY = str
-
-
-# Координаты (широта + долгота)
-class Coordinates(NamedTuple):
-    latitude: float
-    longitude: float
+# Список городов по названию страны
+CITY_ITEMS = tuple
 
 
 # Устанавливаем локацию (страна + город)
 def set_location(_country: COUNTRY, _city: CITY):
+    # Определяем, существует ли страна
+    _define_the_country(_country)
+    # Определяем, существует ли город
+    _define_the_city(_city)
+
+
+# Определяем страну
+def _define_the_country(_country: COUNTRY) -> None:
     global NAME_COUNTRY
-    global NAME_CITY
-    NAME_COUNTRY = _country
-    NAME_CITY = _city
-    # Получаем кортеж городов по ключу (страна)
-    country_item = LOCATION_STORAGE.get(NAME_COUNTRY)
-    print(country_item)
-    # Если название города существует (по ключу - страна)
-    if NAME_CITY in country_item:
-        position = country_item.index(NAME_CITY)
-        NAME_CITY = country_item[position]
-    # Если название города НЕсуществует (по ключу - страна)
+    global CITY_ITEMS
+    # Если страна существует, то получаем список городов по ключу (страна)
+    if _country in LOCATION_STORAGE.keys():
+        NAME_COUNTRY = _country
+        CITY_ITEMS = LOCATION_STORAGE[_country]
     else:
-        NAME_CITY = "Такого города нет"
-        # ===== Будет исключение ===== #
-        pass
+        raise ErrorCountryDoesNotExist
 
 
-# Получение координал по локации (страна + город)
-# def get_gps_coordinates(_country: COUNTRY, _city: CITY) -> Coordinates:
+# Определяем город
+def _define_the_city(_city: CITY) -> None:
+    global NAME_CITY
+    global CITY_ITEMS
+    # Если город существует, то выбираем нужный город
+    if _city in CITY_ITEMS:
+        index = CITY_ITEMS.index(_city)
+        NAME_CITY = CITY_ITEMS[index]
+    else:
+        raise ErrorCityDoesNotExist
+
+
+# Получение координат
 def get_gps_coordinates() -> Coordinates:
+    latitude, longitude = _get_nominatim_coordinates()
+    # Возвращаемое значение :params: широта + долгота
+    return Coordinates(
+        latitude=latitude,
+        longitude=longitude
+    )
+
+
+# Получаем некоторые данные с помощью Nominatim
+def _get_nominatim() -> Nominatim:
+    # Возвращаемое значение :params: данные по локации и прочее
+    return Nominatim(user_agent="tutorial")
+
+
+# Получаем координаты по местоположению с помощью Nominatim
+def _get_nominatim_coordinates() -> Coordinates:
     global NAME_COUNTRY
     global NAME_CITY
-    # Получаем данные по локации и прочее
-    data = Nominatim(user_agent="tutorial")
+    data = _get_nominatim()
     # Получаем словарь данных по местоположению
-    location = data.geocode("%s, %s" % (NAME_COUNTRY, NAME_CITY)).raw
+    data_list = data.geocode("%s, %s" % (NAME_COUNTRY, NAME_CITY))
+    # Если данных нет (так как НЕкорректная страна/город)
+    if data_list is not None:
+        data_list = data_list.raw
+    else:
+        raise ErrorCantGetCoordinates
     # Возвращаемое значение :params: широта + долгота
-    return Coordinates(latitude=location["lat"], longitude=location["lon"])
-
-
-# from enum import Enum
-#
-# class GPSLocations(Enum):
-#     US_RUSSIA = ("Russian", "sdlikg", "dg")
-#     RU_RUSSIA = ("Россия",)
-#
-#
-# for item in GPSLocations:
-#     print(item.name, item.value[0])
+    return Coordinates(
+        latitude=data_list["lat"],
+        longitude=data_list["lon"]
+    )

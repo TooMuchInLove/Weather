2a1acce16158711ad04d65bc44a2c84b70e9f65e
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-19 16:05:16 +0300

    обновление try/except

--- weather.py
+++ weather.py
@@ -10,6 +10,7 @@ from weather_output import output_weather
 from exceptions import ErrorCountryDoesNotExist
 from exceptions import ErrorCityDoesNotExist
 from exceptions import ErrorCantGetCoordinates
+from exceptions import ErrorApiService
 
 
 # Завершаем работу программы
@@ -19,27 +20,35 @@ def _exit_program():
 
 def weather():
     # Страна, город
-    country, city = "Россия", "Москва"
+    country, city = "Ирландия", "Дублин"
     try:
         # Определяем местоположение
         set_location(country, city)
         # Определяем координаты
         coordinates = get_gps_coordinates()
         # Получаем данные о погоде
-        # print(get_weather(coordinates))
+        other_data = get_weather(coordinates)
         # Данные о погоде
-        weather_data = (
-            country,
-            city,
-            coordinates.latitude,
-            coordinates.longitude,
-        )
-        output_weather(weather_data)
+        weather_types = {
+            "date_now": other_data.date_now,
+            "country": country,
+            "city": city,
+            "temperature": other_data.temperature,
+            "humidity": other_data.humidity,
+            "wind_speed": other_data.wind_speed,
+            "type": other_data.weather_type,
+            "description": other_data.description,
+            "sunrise": other_data.sunrise,
+            "sunset": other_data.sunset,
+        }
+        output_weather(weather_types)
     except ErrorCountryDoesNotExist:
         _exit_program()
     except ErrorCityDoesNotExist:
         _exit_program()
     except ErrorCantGetCoordinates:
+        _exit_program()
+    except ErrorApiService:
         _exit_program()
 
 

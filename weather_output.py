107415acd07fbf019a2986d771535b96ea7492f1
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-19 20:36:01 +0300

    два варианта вывода данных о погоде

--- weather_output.py
+++ weather_output.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+# Локальный модули
+from weather_api_service import Weather
 
-def output_weather(_weather_types: dict):
-    # Данные для вывода
-    weather_types = _weather_types
-    print("""
+
+def output_weather(_weather: Weather) -> str:
+    return """
     %s
-    %s, %s
+    %s
         %s°C
     %s, %s
     |   Влажность: %s%%
@@ -14,16 +15,35 @@ def output_weather(_weather_types: dict):
     |   Восход: %s
     |   Закат: %s
     """ % (
-        weather_types["date_now"],
-        weather_types["country"],
-        weather_types["city"],
-        weather_types["temperature"],
+        _weather.date_now,
+        _weather.place,
+        _weather.temperature,
+        _weather.weather_type,
+        _weather.description,
+        _weather.humidity,
+        _weather.wind_speed,
+        _weather.sunrise,
+        _weather.sunset
+    )
 
-        weather_types["type"],
-        weather_types["description"],
-        weather_types["humidity"],
-        weather_types["wind_speed"],
-
-        weather_types["sunrise"],
-        weather_types["sunset"],
-    ))
+def output_weather_colors(_weather: Weather) -> str:
+    return """
+    \033[31m%s\033[0m
+    %s
+        \033[36m %s°C \033[0m
+    %s, %s
+    \033[31m|\033[0m   Влажность: \033[34m%s%%\033[0m
+    \033[31m|\033[0m   Скорость ветра: \033[34m%s\033[0m м/с
+    \033[31m|\033[0m   Восход: \033[34m%s\033[0m
+    \033[31m|\033[0m   Закат: \033[34m%s\033[0m
+    """ % (
+        _weather.date_now,
+        _weather.place,
+        _weather.temperature,
+        _weather.weather_type,
+        _weather.description,
+        _weather.humidity,
+        _weather.wind_speed,
+        _weather.sunrise,
+        _weather.sunset
+    )

25373c001c43ad721253731395906f5020a39e0b
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-19 20:34:36 +0300

    немного поменял структура

--- weather.py
+++ weather.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3.11
 # -*- coding: utf-8 -*-
 
+# Определяем текущую директорию
+from pathlib import Path
+
 # Локальный модули
 from weather_gps import set_location
 from weather_gps import get_gps_coordinates
 from weather_api_service import get_weather
 from weather_output import output_weather
+from logging_ import save_weather, TXTFileStorage
 # Локальные исключения
 from exceptions import ErrorCountryDoesNotExist
 from exceptions import ErrorCityDoesNotExist
@@ -13,48 +17,28 @@ from exceptions import ErrorCantGetCoordinates
 from exceptions import ErrorApiService
 
 
-# Завершаем работу программы
-def _exit_program():
-    exit(1)
-
-
-def weather():
+def main():
     # Страна, город
-    country, city = "Россия", "Мурино"
+    country, city = "Нидерланды", "Амстердам"
     try:
         # Определяем местоположение
         set_location(country, city)
         # Определяем координаты
         coordinates = get_gps_coordinates()
         # Получаем данные о погоде
-        other_data = get_weather(coordinates)
-        # Данные о погоде
-        weather_types = {
-            "date_now": other_data.date_now,
-            "country": country,
-            "city": city,
-            "temperature": other_data.temperature,
-            "humidity": other_data.humidity,
-            "wind_speed": other_data.wind_speed,
-            "type": other_data.weather_type,
-            "description": other_data.description,
-            "sunrise": other_data.sunrise,
-            "sunset": other_data.sunset,
-        }
+        weather_types = get_weather(coordinates, country, city)
         # Отображаем данные
-        output_weather(weather_types)
+        print(output_weather(weather_types))
+        # Логгирование в текстовый документ
+        save_weather(weather_types, TXTFileStorage(Path.cwd()/"logging.txt"))
     except ErrorCountryDoesNotExist:
-        _exit_program()
+        exit(1)
     except ErrorCityDoesNotExist:
-        _exit_program()
+        exit(1)
     except ErrorCantGetCoordinates:
-        _exit_program()
+        exit(1)
     except ErrorApiService:
-        _exit_program()
-
-
-def main():
-    weather()
+        exit(1)
 
 
 if __name__ == "__main__":

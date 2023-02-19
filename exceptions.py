b8f26865dae3f2d5f8df556c1da5bf2cd7dd0790
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-02-19 03:26:04 +0300

    добавление исключений

---
+++ exceptions.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+class ErrorCountryDoesNotExist(Exception):
+    """ Ошибка: страна не существует """
+    def __init__(self):
+        self.__error_message = "Извините! Не смог найти такую страну."
+        self.__error()
+
+    def __error(self):
+        print(self.__error_message)
+
+
+class ErrorCityDoesNotExist(Exception):
+    """ Ошибка: город не существует """
+    def __init__(self):
+        self.__error_message = "Извините! Не смог найти такой город."
+        self.__error()
+
+    def __error(self):
+        print(self.__error_message)
+
+
+class ErrorCantGetCoordinates(Exception):
+    """ Ошибка: не смогли получить координаты """
+    def __init__(self):
+        self.__error_message = "Извините! Не смог получить координаты."
+        self.__error()
+
+    def __error(self):
+        print(self.__error_message)

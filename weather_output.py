463b11fb74756ccbaff735a9531e2b76613898a3
Fyodorov Dmitry <59337840+TooMuchInLove@users.noreply.github.com>
2023-03-24 18:14:02 +0300

    Update weather_output.py

--- weather_output.py
+++ weather_output.py
@@ -25,25 +25,3 @@ def output_weather(_weather: Weather) -> str:
         _weather.sunrise,
         _weather.sunset
     )
-
-def output_weather_colors(_weather: Weather) -> str:
-    return """
-    \033[31m%s\033[0m
-    %s
-        \033[36m %s°C \033[0m
-    %s, %s
-    \033[31m|\033[0m   Влажность: \033[34m%s%%\033[0m
-    \033[31m|\033[0m   Скорость ветра: \033[34m%s\033[0m м/с
-    \033[31m|\033[0m   Восход: \033[34m%s\033[0m
-    \033[31m|\033[0m   Закат: \033[34m%s\033[0m
-    """ % (
-        _weather.date_now,
-        _weather.place,
-        _weather.temperature,
-        _weather.weather_type,
-        _weather.description,
-        _weather.humidity,
-        _weather.wind_speed,
-        _weather.sunrise,
-        _weather.sunset
-    )

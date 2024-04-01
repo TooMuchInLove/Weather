22858ba0bb3e160b2319f8422ff24171e518b604
TooMuchInLove <fedorov.ss-4554@yandex.ru>
2024-04-02 02:26:00 +0300

    ренейминг модуля

--- src/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .weather_api_service import *
-
-__all__ = ("Weather", "get_weather",)
--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,4 +1,4 @@
-from .config import *
+from .base import *
 
 __all__ = ("settings_app", "images_app", "EMPTY", "LOCATION_STORAGE",
            "LANGUAGE", "LOGS_PATH", "OPENWEATHER_URL",)
